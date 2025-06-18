e98d8f4850205cd3b76a920cbcd709f979823f8a
Daniil <142323455+Mag1sstr@users.noreply.github.com>
2025-06-18 22:42:19 +0500

    create Button UI element

--- src/components/blocks/Stages/CartStage/CartStage.tsx
+++ src/components/blocks/Stages/CartStage/CartStage.tsx
@@ -1,64 +1,70 @@
+import Button from "../../../elements/Button/Button";
 import Select from "../../../elements/Select/Select";
 import styles from "./CartStage.module.scss";
 function CartStage() {
   return (
-    <div className={styles.wrapper}>
-      <h3 className={styles.title}>Корзина</h3>
-      <div className={styles.inner}>
-        <div className={styles.details}>
-          <p>Наименование товара</p>
-          <p>Количество</p>
-          <p>Цена</p>
-        </div>
-        <div className={styles.col}>
-          <div className={styles.item}>
-            <div className={styles.info}>
-              <p className={styles.name}>
-                Компрессор кондиционера Hyundai Tucson, Kia Sportage
-                97701-2E300FD; 0935-03se; Kia Sportage 97701-2E300FD; 0935-02
-              </p>
-              <div className={styles.info__row}>
-                Артикул: AC97701 <span>Удалить из корзины</span>
-              </div>
-            </div>
-            <div className={styles.counter}>
-              <div className={styles.counter__row}>
-                <button>-</button>
-                <div className={styles.count}>1</div>
-                <button>+</button>
-              </div>
-            </div>
-            <div className={styles.price}>110 999 тг</div>
+    <>
+      <div className={styles.wrapper}>
+        <h3 className={styles.title}>Корзина</h3>
+        <div className={styles.inner}>
+          <div className={styles.details}>
+            <p>Наименование товара</p>
+            <p>Количество</p>
+            <p>Цена</p>
           </div>
-          <div className={styles.item}>
-            <div className={styles.info}>
-              <p className={styles.name}>
-                Компрессор кондиционера Hyundai Tucson, Kia Sportage
-                97701-2E300FD; 0935-03se; Kia Sportage 97701-2E300FD; 0935-02
-              </p>
-              <div className={styles.info__row}>
-                Артикул: AC97701 <span>Удалить из корзины</span>
+          <div className={styles.col}>
+            <div className={styles.item}>
+              <div className={styles.info}>
+                <p className={styles.name}>
+                  Компрессор кондиционера Hyundai Tucson, Kia Sportage
+                  97701-2E300FD; 0935-03se; Kia Sportage 97701-2E300FD; 0935-02
+                </p>
+                <div className={styles.info__row}>
+                  Артикул: AC97701 <span>Удалить из корзины</span>
+                </div>
               </div>
-            </div>
-            <div className={styles.counter}>
-              <div className={styles.counter__row}>
-                <button>-</button>1<button>+</button>
+              <div className={styles.counter}>
+                <div className={styles.counter__row}>
+                  <button>-</button>
+                  <div className={styles.count}>1</div>
+                  <button>+</button>
+                </div>
               </div>
+              <div className={styles.price}>110 999 тг</div>
             </div>
-            <div className={styles.price}>110 999 тг</div>
+            <div className={styles.item}>
+              <div className={styles.info}>
+                <p className={styles.name}>
+                  Компрессор кондиционера Hyundai Tucson, Kia Sportage
+                  97701-2E300FD; 0935-03se; Kia Sportage 97701-2E300FD; 0935-02
+                </p>
+                <div className={styles.info__row}>
+                  Артикул: AC97701 <span>Удалить из корзины</span>
+                </div>
+              </div>
+              <div className={styles.counter}>
+                <div className={styles.counter__row}>
+                  <button>-</button>1<button>+</button>
+                </div>
+              </div>
+              <div className={styles.price}>110 999 тг</div>
+            </div>
           </div>
-        </div>
-        <div className={styles.bottom}>
-          <div className={styles.payment}>
-            <p>Способ оплаты</p>
-            <Select title="Оплата при получении" />
-          </div>
-          <div className={styles.total}>
-            Итого к оплате: <span>206 998 тг</span>
+          <div className={styles.bottom}>
+            <div className={styles.payment}>
+              <p>Способ оплаты</p>
+              <Select title="Оплата при получении" />
+            </div>
+            <div className={styles.total}>
+              <p>Итого к оплате:</p> <span>206 998 тг</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <Button width={350} height={50} end>
+        Оформить заказ
+      </Button>
+    </>
   );
 }
 
---
+++ src/components/elements/Button/Button.tsx
@@ -0,0 +1,47 @@
+import styles from "./Button.module.scss";
+
+interface IProps {
+  width?: number | string;
+  height?: number | string;
+  fontSize?: number | string;
+  fontWeight?: number;
+  children: React.ReactNode;
+  start?: boolean;
+  center?: boolean;
+  end?: boolean;
+}
+
+function Button({
+  children,
+  fontSize = 18,
+  fontWeight = 600,
+  width,
+  height,
+  start,
+  center,
+  end,
+}: IProps) {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: start
+          ? "flex-start"
+          : center
+          ? "center"
+          : end
+          ? "flex-end"
+          : "",
+      }}
+    >
+      <button
+        className={styles.btn}
+        style={{ fontSize, fontWeight, width, height }}
+      >
+        {children}
+      </button>
+    </div>
+  );
+}
+
+export default Button;
