ed229a791374bf546385bae817cfc64b199905e3
Daniil <142323455+Mag1sstr@users.noreply.github.com>
2025-06-26 04:43:45 +0500

    create cartSlice

---
+++ src/store/slices/cartSlice.ts
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface CartState {
+  count: number;
+}
+
+const initialState: CartState = {
+  count: 0,
+};
+
+const cartSlice = createSlice({
+  name: "cartSlice",
+  initialState,
+  reducers: {
+    setCartCount(state, action) {
+      state.count = action.payload;
+    },
+  },
+});
+
+export const { setCartCount } = cartSlice.actions;
+
+export default cartSlice.reducer;
